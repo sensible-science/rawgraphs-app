44205a64af8d74d34814265b5981c5f4ce058847
Martin Fenner <martin.fenner@datacite.org>
2021-04-06 21:04:26 +0200

    rename to front matter domain

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <Navbar bg="white" expand="lg" sticky="top" className={styles.navbar}>
       <Navbar.Brand href="/">
-        <b>Sensible Science</b>
+        <b>Front Matter</b>
         <span className="text-primary"> Graphs</span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
